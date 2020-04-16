8b439e55f80b6a10337336d46db26bbf8092887d
leonardo534 <leonardoredes534@gmail.com>
2020-04-16 19:46:08 -0300

    Função adicionada ao botao Entrar

--- app/src/main/java/com/leonardosilva/spotifyclone/MainActivity.java
+++ app/src/main/java/com/leonardosilva/spotifyclone/MainActivity.java
@@ -1,14 +1,36 @@
 package com.leonardosilva.spotifyclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.leonardosilva.spotifyclone.fragments.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonLogin;
+    private LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginFragment = new LoginFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameLayout, loginFragment);
+                transaction.commit();
+
+            }
+        });
+
     }
 }
