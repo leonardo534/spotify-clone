d4b66d6490bdde6db0ab87bb8bc909c2ac0a699f
leonardo534 <leonardoredes534@gmail.com>
2020-04-17 17:19:13 -0300

    Função de voltar para fragment anterior adicionado ao imageButtonVoltar

--- app/src/main/java/com/leonardosilva/spotifyclone/fragments/LoginFragment.java
+++ app/src/main/java/com/leonardosilva/spotifyclone/fragments/LoginFragment.java
@@ -3,10 +3,13 @@ package com.leonardosilva.spotifyclone.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.leonardosilva.spotifyclone.R;
 
@@ -14,6 +17,8 @@ import com.leonardosilva.spotifyclone.R;
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
+
+    private ImageButton imageButtonVoltar;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -24,6 +29,16 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        imageButtonVoltar = view.findViewById(R.id.imageButtonVoltar);
+
+        imageButtonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+        return view;
     }
 }
