6b6f54652e3536431304d79d39ffb356f4bfe189
leonardo534 <leonardoredes534@gmail.com>
2020-04-23 01:16:58 -0300

    RecyclerView configurado para mostrar os musicas tocadas recentemente

--- app/src/main/java/com/leonardosilva/spotifyclone/fragments/NavHomeFragment.java
+++ app/src/main/java/com/leonardosilva/spotifyclone/fragments/NavHomeFragment.java
@@ -3,12 +3,19 @@ package com.leonardosilva.spotifyclone.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.leonardosilva.spotifyclone.R;
+import com.leonardosilva.spotifyclone.adapter.AdapterTocadaRecente;
+import com.leonardosilva.spotifyclone.model.TocadaRecente;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,11 +26,42 @@ public class NavHomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    private RecyclerView recyclerTocadasRecente;
+    private List<TocadaRecente> tocadaRecenteList = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_nav_home, container, false);
+        recyclerTocadasRecente = view.findViewById(R.id.recyclerTocadasRecente);
+
+        //ADAPTER
+        AdapterTocadaRecente adapterTocadaRecente = new AdapterTocadaRecente(tocadaRecenteList);
+
+        //RECYCLERVIEW
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerTocadasRecente.setLayoutManager(layoutManager);
+        recyclerTocadasRecente.setHasFixedSize(true);
+        recyclerTocadasRecente.setAdapter(adapterTocadaRecente);
+
+
+        listarTocadasRecente();
+        return view;
+    }
+
+    private void listarTocadasRecente() {
+        TocadaRecente tocadaRecente = new TocadaRecente("Titulo 1", R.mipmap.img_capa_foreground);
+        this.tocadaRecenteList.add(tocadaRecente);
+        tocadaRecente = new TocadaRecente("Titulo 1", R.mipmap.img_capa_foreground);
+        this.tocadaRecenteList.add(tocadaRecente);
+        tocadaRecente = new TocadaRecente("Titulo 1", R.mipmap.img_capa_foreground);
+        this.tocadaRecenteList.add(tocadaRecente);
+        tocadaRecente = new TocadaRecente("Titulo 1", R.mipmap.img_capa_foreground);
+        this.tocadaRecenteList.add(tocadaRecente);
+        tocadaRecente = new TocadaRecente("Titulo 1", R.mipmap.img_capa_foreground);
+        this.tocadaRecenteList.add(tocadaRecente);
+        tocadaRecente = new TocadaRecente("Titulo 1", R.mipmap.img_capa_foreground);
+        this.tocadaRecenteList.add(tocadaRecente);
+
     }
 }
