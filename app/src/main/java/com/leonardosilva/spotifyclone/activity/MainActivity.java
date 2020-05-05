f96427be9bffa56de6f73bc7b9b2a8e2cea0d7b9
leonardo534 <leonardoredes534@gmail.com>
2020-05-05 01:16:33 -0300

    Ao clicar no botao entrar ele chama a activity LoginActivity

--- app/src/main/java/com/leonardosilva/spotifyclone/activity/MainActivity.java
+++ app/src/main/java/com/leonardosilva/spotifyclone/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.leonardosilva.spotifyclone.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
             }
         });
 
