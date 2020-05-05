a780317c5bf1964ca0a2b80235d4d7737033023e
leonardo534 <leonardoredes534@gmail.com>
2020-05-05 01:15:18 -0300

    Ao clicar no botao voltar ele chama a activity MusicasActivity

--- app/src/main/java/com/leonardosilva/spotifyclone/activity/ConfiguracaoActivity.java
+++ app/src/main/java/com/leonardosilva/spotifyclone/activity/ConfiguracaoActivity.java
@@ -2,15 +2,34 @@ package com.leonardosilva.spotifyclone.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.leonardosilva.spotifyclone.R;
+import com.leonardosilva.spotifyclone.fragments.NavHomeFragment;
 
 public class ConfiguracaoActivity extends AppCompatActivity {
+    private ImageButton imageButtonVoltarConfiguracao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracao);
+        inicializarComponentes();
+
+        imageButtonVoltarConfiguracao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MusicasActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void inicializarComponentes() {
+        imageButtonVoltarConfiguracao = findViewById(R.id.imageButtonVoltarConfiguracao);
     }
 }
