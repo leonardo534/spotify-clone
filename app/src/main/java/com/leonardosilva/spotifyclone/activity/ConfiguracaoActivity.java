0f2916ff98242a1fcfbac5e2ca7f7aefd92d2557
leonardo534 <leonardoredes534@gmail.com>
2020-05-04 23:55:36 -0300

    Fragments substituidos pelas Activitys

---
+++ app/src/main/java/com/leonardosilva/spotifyclone/activity/ConfiguracaoActivity.java
@@ -0,0 +1,16 @@
+package com.leonardosilva.spotifyclone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leonardosilva.spotifyclone.R;
+
+public class ConfiguracaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracao);
+    }
+}
---
+++ app/src/main/java/com/leonardosilva/spotifyclone/activity/RecuperarSenhaActivity.java
@@ -0,0 +1,16 @@
+package com.leonardosilva.spotifyclone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leonardosilva.spotifyclone.R;
+
+public class RecuperarSenhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_senha);
+    }
+}
