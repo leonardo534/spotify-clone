e6b78bef5f0ea5c6ff879ce33461c9be2ac0cc91
leonardo534 <leonardoredes534@gmail.com>
2020-05-05 00:16:54 -0300

    Adicionado função de voltar ao ImageButtonVoltar

--- app/src/main/java/com/leonardosilva/spotifyclone/activity/LoginActivity.java
+++ app/src/main/java/com/leonardosilva/spotifyclone/activity/LoginActivity.java
@@ -2,15 +2,33 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.leonardosilva.spotifyclone.R;
 
  public class LoginActivity extends AppCompatActivity {
+    private ImageButton imageButtonVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        inicializarComponentes();
+
+        imageButtonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void inicializarComponentes(){
+        imageButtonVoltar = findViewById(R.id.imageButtonVoltar);
     }
 }
