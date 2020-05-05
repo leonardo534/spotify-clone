baf06e0f662df4df5c5e2d9231b6986cf405ddd5
leonardo534 <leonardoredes534@gmail.com>
2020-05-05 00:29:21 -0300

    Adicionado verificação de campos

--- app/src/main/java/com/leonardosilva/spotifyclone/activity/LoginActivity.java
+++ app/src/main/java/com/leonardosilva/spotifyclone/activity/LoginActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.leonardosilva.spotifyclone.R;
 
  public class LoginActivity extends AppCompatActivity {
     private ImageButton imageButtonVoltar;
+    private Button buttonLoginEmail;
+    private EditText editTextEmail;
+    private EditText editTextSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +33,53 @@ import com.leonardosilva.spotifyclone.R;
             }
         });
 
+        editTextEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                verificarStatus();
+            }
+        });
+
+        editTextSenha.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                verificarStatus();
+            }
+        });
+
     }
 
     private void inicializarComponentes(){
         imageButtonVoltar = findViewById(R.id.imageButtonVoltar);
+        buttonLoginEmail = findViewById(R.id.buttonLoginEmail);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextSenha = findViewById(R.id.editTextSenha);
+    }
+
+    private void verificarStatus(){
+        if(editTextEmail.getText().length() == 0 || editTextSenha.getText().length() == 0){
+            buttonLoginEmail.setEnabled(false);
+        }else{
+            buttonLoginEmail.setEnabled(true);
+        }
+
     }
 }
