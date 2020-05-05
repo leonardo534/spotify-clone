6102837801fcda4d110f3f4b4b0a667e7943fdf6
leonardo534 <leonardoredes534@gmail.com>
2020-05-05 00:02:59 -0300

    Apagado objeto button

--- app/src/main/java/com/leonardosilva/spotifyclone/activity/MainActivity.java
+++ app/src/main/java/com/leonardosilva/spotifyclone/activity/MainActivity.java
@@ -1,18 +1,15 @@
 package com.leonardosilva.spotifyclone.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.leonardosilva.spotifyclone.R;
-import com.leonardosilva.spotifyclone.fragments.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonLogin;
-    private LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +21,6 @@ public class MainActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginFragment = new LoginFragment();
-
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.frameLayout, loginFragment);
-                transaction.addToBackStack("pilha");
-                transaction.commit();
 
             }
         });
