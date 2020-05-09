9e974a2f920308ceffc013fe944486eed57e2c91
leonardo534 <leonardoredes534@gmail.com>
2020-05-08 23:51:17 -0300

    Adicionado ações ao bottomMenu

--- app/src/main/java/com/leonardosilva/spotifyclone/activity/ConfiguracaoActivity.java
+++ app/src/main/java/com/leonardosilva/spotifyclone/activity/ConfiguracaoActivity.java
@@ -1,16 +1,25 @@
 package com.leonardosilva.spotifyclone.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.leonardosilva.spotifyclone.R;
 import com.leonardosilva.spotifyclone.fragments.NavHomeFragment;
+import com.leonardosilva.spotifyclone.fragments.NavLibraryFragment;
+import com.leonardosilva.spotifyclone.fragments.NavPremiumFragment;
+import com.leonardosilva.spotifyclone.fragments.NavSearchFragment;
 
 public class ConfiguracaoActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationConfigurações;
     private ImageButton imageButtonVoltarConfiguracao;
 
     @Override
@@ -31,5 +40,40 @@ public class ConfiguracaoActivity extends AppCompatActivity {
 
     private void inicializarComponentes() {
         imageButtonVoltarConfiguracao = findViewById(R.id.imageButtonVoltarConfiguracao);
+        bottomNavigationConfigurações = findViewById(R.id.nav_menu);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragmentSelected = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            fragmentSelected = new NavHomeFragment();
+                            break;
+
+                        case R.id.nav_search:
+                            Intent intent = new Intent(getApplicationContext(), MusicasActivity.class);
+                            startActivity(intent);
+                            fragmentSelected = new NavSearchFragment();
+                            break;
+
+                        case R.id.nav_library:
+                            fragmentSelected = new NavLibraryFragment();
+                            break;
+
+                        case R.id.nav_premium:
+                            fragmentSelected = new NavPremiumFragment();
+                            break;
+                        default:
+                            fragmentSelected = new NavHomeFragment();
+                    }
+
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frameLayoutMusicas, fragmentSelected);
+                    transaction.commit();
+                    return true;
+                }
+            };
 }
